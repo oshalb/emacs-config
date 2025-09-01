1ebc2958a78235780bf340ee366c19f53affe5b1
Oshal Borkar <mail@oshalborkar.dev>
2025-09-01 11:44:10 +0530

    Configured packages for basic usage. Configured theme and ui settings.

--- early-init.el
+++ early-init.el
@@ -1,1 +1,29 @@
+;; Disable built-in package.el at startup
 (setq package-enable-at-startup nil)
+
+;; Bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package via straight.el
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;; Hide title bar on macOS
+(add-to-list 'default-frame-alist '(undecorated . t))
+(add-to-list 'default-frame-alist '(drag-internal-border . 1))
+(add-to-list 'default-frame-alist '(internal-border-width . 1))
+
--- init.el
+++ init.el
@@ -1,65 +1,81 @@
-;;; Init file for Oshal Borkar
+;;;; ==========================
+;;;; Init file for Oshal Borkar
+;;;; ==========================
 
-;; ------------------------
-;; Basic visual / frame defaults
-;; ------------------------
-;; Disable UI clutter
-(tool-bar-mode -1) ; Hides the tool bar
-(scroll-bar-mode -1) ; Hides scroll bar
-(global-display-line-numbers-mode 1) ; display line numbers
-(setq ring-bell-function 'ignore) ; Removes the annoying alert sound
-(custom-set-variables '(default-frame-alist '((fullscreen . maximized)))) ; Sets the buffer to use fullscreen
-  ;(set-frame-parameter nil 'alpha 60)
-;(add-to-list 'default-frame-alist '(alpha . 60))
-
-;; ------------------------
-;; Bootstrap straight.el (canonical version)
-;; ------------------------
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;;; +++++++++++++++++++++++++++
+;;; Basic visual/frame defaults
+;;; +++++++++++++++++++++++++++
 
 
-;; List of packages installed via Straight.el
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+(defun oshal/setup-ui (frame)
+  "Apply UI settings to FRAME."
+  (with-selected-frame frame
+    (tool-bar-mode -1) ; Hides the tool bar
+    (scroll-bar-mode -1) ; Hides scroll bar
+    (global-display-line-numbers-mode 1) ; Display line numbers
+    (setq ring-bell-function 'ignore) ; Removes the annoying alert sound
+    (set-frame-parameter frame 'fullscreen 'maximized)
+    (set-frame-parameter frame 'undecorated t)
+    (set-frame-font "Source Code Pro-16" nil t)))
 
-;; Use-Package via Straight.el
-(use-package el-patch)
-(use-package meow)
-(use-package auctex
-  :straight t)
-(use-package sr-speedbar
-  :straight t)
-(use-package magit
-  :straight t)
-(use-package vterm
-  :straight t)
-(use-package markdown-mode)
+;; Run for existing frame (when not daemon)
+(when (display-graphic-p)
+  (oshal/setup-ui (selected-frame)))
 
-(add-to-list 'default-frame-alist
-	     '(font . "Source Code Pro-18")) ; Set the font to Source Code Pro with size 18 after loading packages
+;; Run for new frames (daemon + emacsclient)
+(add-hook 'after-make-frame-functions #'oshal/setup-ui)
 
-;; Meow Keybindings
+;;; +++++++++++++++++++++++++++++
+;;; Centralized Auto backup/saves
+;;; +++++++++++++++++++++++++++++
 
-(defun meow-setup ()
-  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+;; Base cache dir
+(defconst emacs-cache-dir "/Users/oshalb/.cache/emacs")
+(defconst emacs-auto-save-dir (concat emacs-cache-dir "/auto-saves/"))
+(defconst emacs-backup-dir (concat emacs-cache-dir "/backups/"))
+
+;; Ensure dirs exist
+(make-directory emacs-auto-save-dir t)
+(make-directory emacs-backup-dir t)
+
+;; --- Auto-saves (#...#) ---
+(setq auto-save-default t
+      auto-save-timeout 10
+      auto-save-interval 10
+      auto-save-file-name-transforms
+      `((".*" ,(concat emacs-auto-save-dir "\\1") t)))
+
+;; --- Backups (~) ---
+(setq backup-directory-alist `(("." . ,emacs-backup-dir))
+      version-control t
+      kept-new-versions 10
+      kept-old-versions 2
+      delete-old-versions t
+      backup-by-copying t)
+
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++
+;;; Package conifigurations (Use-Package via Straight.el)
+;;; +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+(use-package zenburn-theme
+  :config
+  (load-theme 'zenburn t))
+
+(use-package rainbow-delimiters
+  :config
+  (rainbow-delimiters-mode))
+
+(use-package meow
+  :init
+  (meow-global-mode 1)
+  :config
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty) ; Qwerty Layout
+  
   (meow-motion-overwrite-define-key
    '("j" . meow-next)
    '("k" . meow-prev)
    '("<escape>" . ignore))
+
   (meow-leader-define-key
    ;; Use SPC (0-9) for digit arguments.
    '("1" . meow-digit-argument)
@@ -77,11 +93,10 @@
    '("f" . find-file)
    '("s" . save-buffer)
    '("qq" . kill-emacs)
-   '("SPC" . (lambda () (interactive) (call-interactively 'execute-extended-command)))
    '("bs" . switch-to-buffer)
    '("bk" . kill-buffer)
-   '("qf" . delete-frame)
-   '("g" . magit-status))
+   '("qf" . delete-frame))
+
   (meow-normal-define-key
    '("0" . meow-expand-0)
    '("9" . meow-expand-9)
@@ -143,39 +158,333 @@
    '("z" . meow-pop-selection)
    '("'" . repeat)
    '("<escape>" . ignore))
+
   (meow-define-keys 'insert
-   '("C-[" . meow-insert-exit)))
+    '("C-[" . meow-insert-exit)))
 
-(meow-setup)
-(meow-global-mode 1)
+(use-package magit)
+(use-package vterm)
+(use-package markdown-mode)
 
 
-;; Speedbar settings
-(setq speedbar-show-unknown-files t)
-(setq speedbar-directory-unshown-regexp "^$")
 
-;; ===== Centralize Backups and Auto-Saves =====
+(use-package projectile
+  :init
+  (setq projectile-project-search-path '("~/Projects/" "~/Projects/git_repos/"))
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+	      ("C-c p" . projectile-command-map))
+  :after meow
+  :config
+  (meow-leader-define-key
+   '("p" . projectile-command-map)))
 
-;; Base cache dir
-(defconst emacs-cache-dir "/Users/oshalb/.cache/emacs")
-(defconst emacs-auto-save-dir (concat emacs-cache-dir "/auto-saves/"))
-(defconst emacs-backup-dir (concat emacs-cache-dir "/backups/"))
+(use-package which-key
+  :config
+  (meow-leader-define-key
+   '("w" . which-key-mode)))
 
-;; Ensure dirs exist
-(make-directory emacs-auto-save-dir t)
-(make-directory emacs-backup-dir t)
+(use-package nerd-icons)
 
-;; --- Auto-saves (#...#) ---
-(setq auto-save-default t
-      auto-save-timeout 10
-      auto-save-interval 10
-      auto-save-file-name-transforms
-      `((".*" ,(concat emacs-auto-save-dir "\\1") t)))
+(use-package doom-modeline
+  :after nerd-icons
+  :init
+  (doom-modeline-mode +1))
 
-;; --- Backups (~) ---
-(setq backup-directory-alist `(("." . ,emacs-backup-dir))
-      version-control t
-      kept-new-versions 10
-      kept-old-versions 2
-      delete-old-versions t
-      backup-by-copying t)
+(use-package vertico
+  ;:custom
+  ;; (vertico-scroll-margin 0) ;; Different scroll margin
+  ;; (vertico-count 20) ;; Show more candidates
+  ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+)
+
+(use-package embark
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+  ;; (context-menu-mode 1)
+  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package treemacs
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-buffer-name-function            #'treemacs-default-buffer-name
+          treemacs-buffer-name-prefix              " *Treemacs-Buffer-"
+          treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                2000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-files-by-mouse-dragging    t
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-project-follow-into-home        nil
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           25
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (when treemacs-python-executable
+      (treemacs-git-commit-diff-mode t))
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  (add-hook 'treemacs-mode-hook
+            (lambda ()
+	      ;; Disable line numbers + scroll bar
+              (display-line-numbers-mode -1)
+              (scroll-bar-mode -1)
+	      ;; Skip treemacs window with C-x o
+              (setq-local window-persistent-parameters
+                          (append window-persistent-parameters
+                                  '((no-other-window . t))))
+              (set-window-parameter (selected-window) 'no-other-window t)))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
+(use-package treemacs-magit
+  :after (treemacs magit))
+
+(use-package company
+  :config
+  (global-company-mode))
+
+(use-package flycheck
+  :config
+  (global-flycheck-mode))
+
+(use-package direnv
+ :config
+ (direnv-mode))
+
+
+(use-package go-mode
+  :hook ((go-mode . eglot-ensure)
+	 (go-mode . (lambda()
+		      (setq tab-width 4
+			    indent-tab-mode 1)
+		      (add-hook 'before-save-hook #'gofmt-before-save nil t))))
+  :config
+  (setq gofmt-command "goimports"))
+
+
+;;; init.el ends here
